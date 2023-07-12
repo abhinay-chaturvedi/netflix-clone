2b2d5486a9d401071d31c493766b26d2c142a5dc
abhinay chaturvedi <abhinaychaturvedi88@gmail.com>
2023-07-12 12:48:41 +0530

    added the env variables

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -8,12 +8,12 @@ const firebaseConfig = {
   // messagingSenderId: "316700975498",
   // appId: "1:316700975498:web:15063159b205c1a349a873",
   // measurementId: "G-2ZGE63ES9F",
-  apiKey: "AIzaSyB6NQSpMPWiKpLS1mdh2AgirYtzhUEPHag",
-  authDomain: "netflix-clone-19ad8.firebaseapp.com",
-  projectId: "netflix-clone-19ad8",
-  storageBucket: "netflix-clone-19ad8.appspot.com",
-  messagingSenderId: "319271540393",
-  appId: "1:319271540393:web:64d98988e4974269f4fbb8"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.messagingSenderId,
+  appId: process.env.appId
 };
 
 firebase.initializeApp(firebaseConfig);
